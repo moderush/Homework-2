7a94511b71e124307822e7da2df6d9ad228a6b53
Vladimir <dratsky@inbox.ru>
2022-03-06 15:58:48 +0300

    zadacha19

--- Homework C#1/Zadacha 6/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.WriteLine("Задача 6. Проверка на четность.");
-Console.WriteLine("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-if (num % 2 == 0){
-    Console.WriteLine("Число является четным");
-}
-else{
-   Console.WriteLine("Число является нечетным"); 
-}
