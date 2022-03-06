494a865e5f8539ec56917a9666a4ad82b3744a2e
Vladimir <dratsky@inbox.ru>
2022-03-06 14:48:11 +0300

    .\HomeworkC#2\

---
+++ HomeworkC#2/zadacha 13/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.");
+Console.Write("Введите натуральное трёхзначное число: ");
+int num = int.Parse(Console.ReadLine());
+d(num);
+void d(int number)
+{
+    if (number < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+    else
+    {
+        while (number > 999)
+        {
+            number = number / 10;
+        }
+        number = number % 10;
+        Console.WriteLine("Третья цифра числа: " + number);
+    }
+}
+
+
+
+
+
+
+
+
